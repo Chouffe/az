7ad75e1400ff3b4eba43f3402ce452d843673bcb
Arthur Caillau <chouffe.caillau@gmail.com>
2014-10-20 15:44:59 -0700

    Finish db layer

--- db.py
+++ db.py
@@ -23,6 +23,7 @@
 
 
 import settings
+import utils
 from pymongo import MongoClient
 
 
@@ -31,3 +32,45 @@ def init_db():
     db connection instance"""
     client = MongoClient()
     return client[settings.db_name]
+
+db = init_db()
+
+
+def create_indexes():
+    """Initializes the indexes on the collections"""
+    db['schemas'].ensure_index('uuid', unique=True)
+    db['datapoints'].ensure_index('uuid')
+
+
+def drop_inndexes():
+    """Drops all the indexes"""
+    db['schemas'].drop_indexes()
+    db['datapoints'].drop_indexes()
+
+# -----------------------
+#       Schemas
+# -----------------------
+
+
+def write_schema(uuid, features):
+    """Given an uuid and a sequence of features,
+    it writes the schema in the db"""
+    data = {'uuid': uuid, 'features': features}
+    return db['schemas'].insert(data)
+
+
+def get_schema(uuid):
+    return db['schemas'].find_one({'uuid': uuid})
+
+# -----------------------
+#       Datapoints
+# -----------------------
+
+def write_datapoint(uuid, features, result):
+    """Given an uuid and a sequence of features,
+    it writes the schema in the db"""
+    data = {'uuid': uuid,
+            'features': features,
+            'result': result,
+            'time': utils.now()}
+    return db['datapoints'].insert(data)
